73b6383dade5b2e5b20f35c599cde0d9265d3608
Qwardelle <akubanzt53k8@gmail.com>
2024-01-29 15:02:36 +0300

    upd js

--- js.js
+++ js.js
@@ -1,7 +1,13 @@
-// let click = document.querySelectorAll(".menu");
-
-// console.log(click);
-
-function click(elem) {
-  console.log(1);
+let menuText = document.querySelectorAll(".menu");
+function displayNone() {
+  menuText.forEach(function addDisplay(i) {
+    i.firstElementChild.style.display = "none";
+  });
 }
+
+menuText.forEach(function clickEvent(i) {
+  i.addEventListener("click", function message() {
+    displayNone();
+    i.firstElementChild.style.display = "block";
+  });
+});
